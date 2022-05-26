fe9a15e0a8bea40308ef468f1054274240c750d8
David Lopez <sebastian.830@outlook.com>
2022-05-25 21:06:40 -0500

    Traer datos al py

--- index.py
+++ index.py
@@ -1,34 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-app = Flask (__name__)      #incializar variable
+app = Flask(__name__)  # incializar variable
 
 
 #funcion de bienvenida
 @app.route('/')
-
 def principal():
     return render_template('index.html')
 
 
-
-
-#main del programa
+@app.route('/add_tareas', methods=['POST'])
+def add_tareas():
+   if request.method == 'POST':
+      Nombre_Tarea = request.form['Nombre_Tarea']
+      Correo = request.form['Correo']
+      #prioridad = request.form['prioridad']
+      print(Nombre_Tarea)
+      print(Correo)
+      #print(prioridad)
+      return 'OK'
+     
+  
+# main del programa
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-
-
-
-@app.route('/')
-def contacto():
-    return render_template(' ')
-
-
-@app.route('/ ')
-def lenguajesdeprogramacion():
-    return render_template(' ')
-
