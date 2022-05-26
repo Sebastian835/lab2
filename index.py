93990dca3fb0f8a5c30ac31b51d2f538bef2069f
David Lopez <sebastian.830@outlook.com>
2022-05-25 21:19:54 -0500

    No muestra mensaje "recivido"

--- index.py
+++ index.py
@@ -1,24 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for         #librerias
 
 app = Flask(__name__)  # incializar variable
 
 
 #funcion de bienvenida
 @app.route('/')
-def principal():
+def index():
     return render_template('index.html')
 
-
-@app.route('/add_tareas', methods=['POST'])
+#funcion para traer las tareas del index add_tareas 
+@app.route('/add_tareas', methods=['POST'])             #  recive atravez del metodo POST    
 def add_tareas():
    if request.method == 'POST':
-      Nombre_Tarea = request.form['Nombre_Tarea']
-      Correo = request.form['Correo']
-      #prioridad = request.form['prioridad']
-      print(Nombre_Tarea)
-      print(Correo)
-      #print(prioridad)
-      return 'OK'
+      Nombre_Tarea = request.form['Nombre_Tarea']       #almacena el nombre de la tarea
+      Correo = request.form['Correo']                  
+      prioridad = request.form['prioridad']  
+      return redirect(url_for('index'))               
+    
      
   
 # main del programa
