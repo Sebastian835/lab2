6c6eb00cd8b14295a76a77fa05daaa29c3476825
David Lopez <sebastian.830@outlook.com>
2022-05-28 20:03:10 -0500

    Final

--- todoapp.py
+++ todoapp.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for  # librerias
 import numpy as np
+import os
 
 app = Flask(__name__)  # incializar variable
 
@@ -15,22 +16,33 @@ correo = []
 prioridad = []
 
 #add_tareas
-@app.route('/', methods=['POST'])  # recive atravez del metodo POST
-def add_tareas():
+@app.route('/enviar', methods=['POST'])  # recive atravez del metodo POST
+def enviar():
    if request.method == 'POST':
       # almacena el nombre de la tarea
       Nombre_Tarea = request.form['Nombre_Tarea']
       Correo_Tarea = request.form['Correo']
       prioridad_Tarea = request.form['prioridad']
+    
+      #si faltan datos no ingresa al array 
+      if(Nombre_Tarea == "" or Correo_Tarea == "" or prioridad_Tarea == "" ):
+          return redirect(url_for('index'))  
 
       #Almacener datos en los arreglos
       tareaNombre.append(Nombre_Tarea)
       correo.append(Correo_Tarea)
       prioridad.append(prioridad_Tarea)
       
-      #tareaNombre.clear()
-      #correo.clear()
-      #prioridad.clear()
+      return redirect(url_for('index'))   
+
+
+
+@app.route('/borrar', methods=['POST']) 
+def borrar():
+    if request.method == 'POST':
+      tareaNombre.clear()
+      correo.clear()
+      prioridad.clear()
       return redirect(url_for('index'))   
 
 # main del programa
